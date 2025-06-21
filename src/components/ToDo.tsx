41a8ade5075fe1f8a5cd8bcaad81a896e6d7fbc1
Sanjayazhagan <sanjayazhagan@gmail.com>
2025-06-22 02:45:37 +0530

    layout problem fix

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -32,7 +32,7 @@ export default function ToDo({id,title,completed,onDelete,onToggle,onName,deadli
         setDeadline(e.target.value);
     }
     return <div className="">
-        <form className=" relative flex items-center m-2 p-6 shadow bg-gray-50 hover:bg-gray-100" onSubmit={(e)=>{e.preventDefault(); setEdit(!edit); onName(id,task); onDeadline(id,Deadline)}}>
+        <form className=" relative flex items-center m-2 p-3 shadow bg-gray-50 hover:bg-gray-100" onSubmit={(e)=>{e.preventDefault(); setEdit(!edit); onName(id,task); onDeadline(id,Deadline)}}>
             <div className="flex items-center">
             <input 
             type="checkbox"
@@ -41,14 +41,14 @@ export default function ToDo({id,title,completed,onDelete,onToggle,onName,deadli
             onChange={HandleChange}
             ></input>
             <div className="flex flex-col">
-            {edit?<input type="text" className="bg-white pt-3 p-2 text-2xl" value={task}onChange={NameChange}></input>:
-            <label onClick={HandleEdit} className={`text-2xl p-2 ml-5 ${complete?"line-through":""}`}>{task}</label>
+            {edit?<input type="text" className="bg-white pt-3 p-2 text-xl" value={task}onChange={NameChange}></input>:
+            <label onClick={HandleEdit} className={`text-xl p-2 ml-2 ${complete?"line-through":""}`}>{task}</label>
             }
-            {edit?<input type="date" className="bg-white pt-3 p-2 text-xl " value={Deadline}onChange={DeadlineChange}></input>:
-            <label onClick={HandleEdit} className={`text-xl ml-15 text-gray-600 ${complete?"line-through":""}`}>{Deadline}</label>
+            {edit?<input type="date" className="bg-white pt-3 p-2 text-lg " value={Deadline}onChange={DeadlineChange}></input>:
+            <label onClick={HandleEdit} className={`text-lg ml-15 text-gray-600 ${complete?"line-through":""}`}>{Deadline}</label>
             }
             </div>
-            {edit&&<button className="bg-indigo-600 text-white p-3 rounded-xl text-2xl">Save</button>}
+            {edit&&<button className="bg-indigo-600 text-white p-2 rounded-xl text-xl">Save</button>}
             </div>
             <div onClick={onDelete} className=" absolute top-0 right-0"><IoIosCloseCircle className="w-7 h-7" /></div>
         </form>
